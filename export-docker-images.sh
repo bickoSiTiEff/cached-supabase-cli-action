c625feeaef549ffbb44762a6b98f79aabd25a4e9
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2022-09-06 21:38:25 +0200

    Skip exporting already exported images

--- export-docker-images.sh
+++ export-docker-images.sh
@@ -11,12 +11,18 @@ IMAGES=$(docker images --format="{{.Repository}}:{{.Tag}}")
 for IMAGE in $IMAGES; do
 
   if grep -Fxq "$IMAGE" <(echo "$EXISTING_IMAGES"); then
-    echo "Skipping image $IMAGE..."
+    echo "Skipping image $IMAGE because it is builtin..."
+    continue
+  fi
+
+  TARGET_FILE_NAME="$(echo "$IMAGE" | base64).tar.gz"
+
+  if [ -f "$TARGET_FILE_NAME" ]; then
+    echo "Skipping image $IMAGE because it is already exported..."
     continue
   fi
 
   echo "Exporting image $IMAGE..."
-  TARGET_FILE_NAME="$(echo "$IMAGE" | base64).tar.gz"
   docker save "$IMAGE" | pigz >"$TARGET_FILE_NAME"
   echo "Exported to $TARGET_FILE_NAME"
 
