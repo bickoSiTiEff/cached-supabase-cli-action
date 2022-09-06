54098e7d93393abc59cd5a2bf02447effea53dc1
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2022-09-06 21:30:32 +0200

    Avoid exporting builtin images

--- export-docker-images.sh
+++ export-docker-images.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+EXISTING_IMAGES=$(cat existing-docker-containers.txt)
+
 mkdir -p supabase-cache
 cd supabase-cache
 
 IMAGES=$(docker images --format="{{.Repository}}:{{.Tag}}")
 
 for IMAGE in $IMAGES; do
+
+  if grep -Fxq "$IMAGE" <(echo "$EXISTING_IMAGES"); then
+    echo "Skipping image $IMAGE..."
+    continue
+  fi
+
   echo "Exporting image $IMAGE..."
   TARGET_FILE_NAME="$(echo "$IMAGE" | base64).tar.gz"
   docker save "$IMAGE" | pigz >"$TARGET_FILE_NAME"
   echo "Exported to $TARGET_FILE_NAME"
+
 done
